de4c8f8fc467cfe04fbb99c89e55eda3221f1983
quanchobi <quanchobi@proton.me>
2024-11-08 21:07:46 -0500

    transmissioN

--- host/server/services/transmission.nix
+++ host/server/services/transmission.nix
@@ -3,5 +3,10 @@
     enable = true;
     settings.download-dir = /mnt/crypt/samba_share/Media/Downloads;
     group = "multimedia";
+    openRPCPort = true;
+    settings = {
+      rpc-bind-address = "0.0.0.0";
+      rpc-whitelist = "100.119.129.119";
+    };
   };
 }
