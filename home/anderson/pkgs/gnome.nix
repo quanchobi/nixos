300eb55af1ee5c7926e11679a2b98b7ea4f7b210
quanchobi <quanchobi@proton.me>
2025-01-13 22:51:13 -0500

    gnome

---
+++ home/anderson/pkgs/gnome.nix
@@ -0,0 +1,31 @@
+{ pkgs, ... }:
+{
+  home.packages = with pkgs; [
+  gnomeExtensions.blur-my-shell
+  gnomeExtensions.dash-to-dock
+  gnomeExtensions.forge
+  gnomeExtensions.user-themes
+  ];
+
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+      enable-hot-corners = false;
+    };
+
+    "org/gnome/shell" = {
+      disable-user-extensions = false;
+      enabled-extensions = with pkgs.gnomeExtensions; [
+        blur-my-shell.extensionUuid
+        forge.extensionUuid
+        user-themes.extensionUuid
+        dash-to-dock.extensionUuid
+        system-monitor.extensionUuid
+      ];
+
+      disabled-extensions = [
+        "native-window-placement@gnome-shell-extensions.gcampax.github.com"
+      ];
+    };
+  };
+}
