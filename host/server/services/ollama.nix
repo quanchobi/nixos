18b7479491ab851835a366478dbd420585070d5f
quanchobi <quanchobi@proton.me>
2025-01-23 06:48:40 +0000

    ollama nixvim update

--- host/server/services/ollama.nix
+++ host/server/services/ollama.nix
@@ -3,15 +3,23 @@
     ollama = {
       enable = true;
       openFirewall = true;
-      loadModels = [ "deepseek-r1:14B" ];
+      host = "[::]";
+      port = 11434;
+      loadModels = [
+        "deepseek-r1:8B"
+        "deepseek-r1:14B"
+        "llama3.1:8b"
+        "llama3.2:3b"
+      ];
     };
 
     open-webui = {
       enable = true;
       openFirewall = true;
       host = "100.87.28.41";
+      port = 8080;
     };
 
-    # comfyui.enable = true; # no cpu support
+    # comfyui.enable = true; # no cpu support :(
   };
 }
