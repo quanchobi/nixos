1ab947bf2658cafb9e1008a3e5ea801acf8bb7ce
quanchobi <quanchobi@proton.me>
2025-01-24 22:57:03 -0500

    nixvim cmp

--- home/anderson/cli/nixvim/plugins/cmp/default.nix
+++ home/anderson/cli/nixvim/plugins/cmp/default.nix
@@ -1,6 +1,6 @@
 {
   imports = [
-    ./blink.nix
+    #./blink.nix
     ./cmp.nix
   ];
 }
--- home/anderson/cli/nixvim/plugins/obsidian.nix
+++ home/anderson/cli/nixvim/plugins/obsidian.nix
@@ -1,27 +1,35 @@
 {
   programs.nixvim = {
-    opts.conceallevel = 2;
-    plugins.obsidian = {
-      enable = true;
-      settings = {
-        workspaces = [
-          {
-            name = "second-brain";
-            path = "~/obsidian/second-brain";
-          }
-          {
-            name = "journal";
-            path = "~/obsidian/journal";
-          }
-          {
-            name = "school";
-            path = "~/obsidian/school";
-          }
-          {
-            name = "website";
-            path = "~/obsidian/website";
-          }
-        ];
+    opts.conceallevel = 2; # necessary for obsidian
+
+    plugins = {
+      obsidian = {
+        enable = true;
+        settings = {
+          workspaces = [
+            {
+              name = "second-brain";
+              path = "~/obsidian/second-brain";
+            }
+            {
+              name = "journal";
+              path = "~/obsidian/journal";
+            }
+            {
+              name = "school";
+              path = "~/obsidian/school";
+            }
+            {
+              name = "website";
+              path = "~/obsidian/website";
+            }
+          ];
+        };
+      };
+
+      render-markdown = {
+        enable = true;
+        settings = { };
       };
     };
   };
