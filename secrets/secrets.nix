cc3a99dc0a1a2ef8f28de65a44e65f486c1aa66e
quanchobi <quanchobi@proton.me>
2025-02-12 20:29:54 +0000

    fix: agenix

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,10 +1,22 @@
 let
-  #framework = "";
-  #desktop = "";
-  #server = "";
+  # users
   anderson = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMltYp0nSf+aRcpKo9hysa2kHTGOiguAMEVzpL6gMgHC anderson@framework";
+
+  # machines
+  server = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM0bEHtDoYFwcdeophpGYxHn15VyP6PGh4qzPfT5ThTb root@server";
+  framework = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFaAsve5f1Jw/zrmGiHrjzi6NcZYtoMa6LQfB3DhsyK3 root@nixos";
+
+  machines = [
+    server
+    framework
+  ];
+
+  users = [
+    anderson
+  ];
+
 in
 {
-  "anthropic-api-key.age".publicKeys = [ anderson ];
-  "cloudflare-dns-challenge-key.age".publicKeys = [ anderson ];
+  "anthropic-api-key.age".publicKeys = users;
+  "cloudflare-dns-challenge-key.age".publicKeys = machines;
 }
