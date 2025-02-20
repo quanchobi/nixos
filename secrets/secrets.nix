1cda60c50a65baa750ad62855f9b9f4cbad25f30
quanchobi <quanchobi@proton.me>
2025-02-20 04:32:32 +0000

    flake.nix refactor

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -5,18 +5,21 @@ let
   # machines
   server = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM0bEHtDoYFwcdeophpGYxHn15VyP6PGh4qzPfT5ThTb root@server";
   framework = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFaAsve5f1Jw/zrmGiHrjzi6NcZYtoMa6LQfB3DhsyK3 root@framework";
+  desktop = "";
+  wsl = "";
 
   machines = [
     server
     framework
+    desktop
+    wsl
   ];
 
   users = [
     anderson
   ];
-
 in
 {
   #"anthropic-api-key.age".publicKeys = users;
-  "cloudflare-api-key.age".publicKeys = machines;
+  "cloudflare-api-key.age".publicKeys = server;
 }
