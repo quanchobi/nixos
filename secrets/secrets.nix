6375ae961f4de55e6491bdd162c8bc90f1e41ac3
quanchobi <quanchobi@proton.me>
2025-02-21 23:02:08 -0500

    Feat: dap + samba client

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -5,14 +5,17 @@ let
   # machines
   server = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM0bEHtDoYFwcdeophpGYxHn15VyP6PGh4qzPfT5ThTb root@server";
   framework = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFaAsve5f1Jw/zrmGiHrjzi6NcZYtoMa6LQfB3DhsyK3 root@framework";
-  desktop = "";
-  wsl = "";
+  desktop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPildc+k+Jku5GdH8/8+0K/OLz89uxYUOMCfH7jOWtcb root@framework";
 
   machines = [
     server
     framework
     desktop
-    wsl
+  ];
+
+  clients = [
+    framework
+    desktop
   ];
 
   users = [
@@ -20,6 +23,7 @@ let
   ];
 in
 {
-  #"anthropic-api-key.age".publicKeys = users;
-  "cloudflare-api-key.age".publicKeys = server;
+  #"anthropic-api-key.age".publicKeys = anderson;
+  #"cloudflare-api-key.age".publicKeys = server;
+  "samba-credentials.age".publicKeys = clients;
 }
