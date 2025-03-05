7eeaa65f3ccea007f0de7677bf51f40b6b5e949b
quanchobi <quanchobi@proton.me>
2025-03-05 09:08:49 -0500

    fix: add desktop to syncthing

--- host/desktop/modules/services/syncthing.nix
+++ host/desktop/modules/services/syncthing.nix
@@ -3,12 +3,12 @@
     enable = true;
     settings = {
       devices = {
-        # framework = {
-        #   addresses = [
-        #     "100.119.124.119"
-        #   ];
-        #   id = "";
-        # };
+        framework = {
+          addresses = [
+            "100.119.124.119"
+          ];
+          id = "Y5MHMRC-3H6CLR6-B73GDCZ-GPRUDE5-N32LTPD-5WA2EEB-7E53PPF-4XJ7WAF";
+        };
       };
     };
   };
